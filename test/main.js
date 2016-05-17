bfe1c0cd2bacb46e95bc951f370587849249a88c
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2016-05-17 22:38:10 +0800

    test

--- test/main.js
+++ test/main.js
@@ -4,8 +4,10 @@ let $fs = require("fs");
 let $cp = require("child_process");
 let rm = require("rimraf");
 
-let exec = command => {
-    return $cp.execSync(command, {cwd: "temp"});
+let exec = (command, options) => {
+    let actualOptions = {cwd: "temp"};
+    Object.assign(actualOptions, options);
+    return $cp.execSync(command, actualOptions);
 };
 
 describe("simple", () => {
@@ -32,5 +34,6 @@ node_modules
 
     $fs.writeFileSync("temp/a.txt", "file body 1\n");
     $fs.writeFileSync("temp/我 你.txt", "文件 2\n");
-    exec("git add . && git commit -m 第二个");
+    exec("git add .");
+    exec("git commit -F -", {input: "第二个\n哈哈"});
 });

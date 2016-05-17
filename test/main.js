47d343327c1cf87ad7d634127d3b8df8e5de09d0
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2016-05-17 08:14:18 +0800

    test

--- test/main.js
+++ test/main.js
@@ -12,6 +12,7 @@ describe("simple", () => {
     rm.sync("temp");
     $fs.mkdirSync("temp");
     exec("git init");
+
     $fs.writeFileSync("temp/.gitignore", String.raw`# OS X
 .DS_Store
 
@@ -28,4 +29,8 @@ node_modules
 *.log
 `);
     exec("git add . && git commit -m init");
+
+    $fs.writeFileSync("temp/a.txt", "file body 1\n");
+    $fs.writeFileSync("temp/我 你.txt", "文件 2\n");
+    exec("git add . && git commit -m 第二个");
 });

9d59a4510030f8718ed21cc6c313a6bdd2baafb0
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2016-06-06 02:25:48 +0800

    test

--- test/main.js
+++ test/main.js
@@ -224,6 +224,22 @@ describe("all", function() {
                 files: "100644 sha256-7dfa8c4d0ae505ae9e5404495eff9d5c04ec13faae715f9dfc77d98b8426a620 .gitignore\n",
                 commitMessage: "init\n"
             });
+            ass.strictEqual(commits[1].locks.length, 1);
+            assBaseLock(commits[1].locks[0], commits[1], {
+                parentLocks: [commits[0].locks[0]],
+                files:
+                    "100644 sha256-7dfa8c4d0ae505ae9e5404495eff9d5c04ec13faae715f9dfc77d98b8426a620 .gitignore\n" +
+                    "100644 sha256-3a7429fbbd37ec336920ca94afdb11116c71144209fec0b07eb7dc3c3bbe4f40 a.txt\n" +
+                    "040000 sha256-0000000000000000000000000000000000000000000000000000000000000000 dir1\n" +
+                    "100644 sha256-bdc26931acfb734b142a8d675f205becf27560dc461f501822de13274fe6fc8a dir1/a.txt\n" +
+                    "100644 sha256-8b410a5102fa5a38ef71e9e7c3f7888a9c029da41cfce2b16fd6f4c062b88030 dir1/b.txt\n" +
+                    "040000 sha256-0000000000000000000000000000000000000000000000000000000000000000 dir2\n" +
+                    "100644 sha256-bdc26931acfb734b142a8d675f205becf27560dc461f501822de13274fe6fc8a dir2/a.txt\n" +
+                    "100644 sha256-77f04111cf23a2831ad5ce51903577bff91b281780e445264368d1c78fab157f dir2/b.txt\n" +
+                    "100644 sha256-38446ff653e68d69e16e8358fefc829c4b8f027d2e00b6c98a40db2a1a255fef dir2/c.txt\n" +
+                    "100644 sha256-d7f6df5b097bcc6a4d11d1b4901f342fe0fd9aca663e7e32c704fe6816a744e5 我 你.txt\n",
+                commitMessage: "第二个\n哈哈\n\n哈哈\n"
+            });
             $fs.mkdirSync("temp/proof");
             runGitlock("proof --all proof");
         });
